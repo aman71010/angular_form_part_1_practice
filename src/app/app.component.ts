0617e5efdeb40fe7c708bf7b192c247cca8400f7
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-06-20 22:08:08 +0530

    uses ngModel(three ways), ngModelGroup, setValue, patchValue, reset  method and uses form data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,37 @@ import { NgForm } from '@angular/forms'
 })
 export class AppComponent {
   @ViewChild('f') sigupForm: NgForm;
+  defaultOption = 'teacher';
+  answer = '';
+  genders = ['male', 'female'];
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.sigupForm.setValue({
+    //   userdata: {
+    //     email: 'aryagupta@gmail.com',
+    //     username: 'arya'
+    //   },
+    //   gender: 'female',
+    //   secret: 'pet',
+    //   answer: 'tommy'
+    // });
+    this.sigupForm.form.patchValue({
+      userdata: {
+        username: suggestedName
+      }
+    });
+  }
+
+  user = {
+    userdata: {
+      username: '',
+      email: ''
+    },
+    gender: '',
+    secret: '',
+    answer: ''
   }
 
   // onSubmit(form: HTMLFormControlsCollection){
@@ -18,7 +46,14 @@ export class AppComponent {
   // }
 
   onSubmit(){
+    this.submitted = true;
     console.log(this.sigupForm);
+    this.user.userdata.username = this.sigupForm.value.userdata.username
+    this.user.userdata.email = this.sigupForm.value.userdata.email
+    this.user.gender = this.sigupForm.value.gender
+    this.user.secret = this.sigupForm.value.secret
+    this.user.answer = this.sigupForm.value.answer
+    this.sigupForm.reset();
   }
 
 }
